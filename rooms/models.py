0f590590d976b0b1993a1265ab2c24c1ceec5318
Junmo Yang <jmyang@logicalart.co.kr>
2020-02-25 19:42:07 +0900

    #0007 #4 Room App 완료

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from django_countries.fields import CountryField
+from core import models as core_models
+from users import models as users_models
 
-# Create your models here.
+
+class AbstractItem(core_models.TimeStampedModel):
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+    class Meta:
+        verbose_name = "Room Type"
+        ordering = ["name"]
+
+
+class Amenity(AbstractItem):
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+
+class Facility(AbstractItem):
+    class Meta:
+        verbose_name_plural = "Facilities"
+
+
+class HouseRule(AbstractItem):
+    class Meta:
+        verbose_name = "House Rule"
+
+
+class Room(core_models.TimeStampedModel):
+    name = models.CharField(max_length=140)
+    description = models.TextField()
+    country = CountryField()
+    city = models.CharField(max_length=80)
+    price = models.IntegerField()
+    address = models.CharField(max_length=140)
+    guests = models.IntegerField()
+    beds = models.IntegerField()
+    bedrooms = models.IntegerField()
+    baths = models.IntegerField()
+    check_in = models.TimeField()
+    check_out = models.TimeField()
+    instant_book = models.BooleanField(default=False)
+    host = models.ForeignKey(
+        users_models.User, on_delete=models.CASCADE
+    )  # CASCADE 폭포수. 다른 필드에 영향을 줌
+    room_type = models.ForeignKey(
+        RoomType, blank=True, on_delete=models.SET_NULL, null=True
+    )
+    amenities = models.ManyToManyField(Amenity, blank=True)
+    facilities = models.ManyToManyField(Facility, blank=True)
+    house_rules = models.ManyToManyField(HouseRule, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Photo(core_models.TimeStampedModel):
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
