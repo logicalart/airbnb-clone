9e1829ea771974fe9f01176da3b5d922c3851b99
Junmo Yang <jmyang@logicalart.co.kr>
2020-04-14 16:46:49 +0900

    #0027 #24 Reservations and Reviews

--- core/models.py
+++ core/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from . import managers
 
 
 class TimeStampedModel(models.Model):
 
     created = models.DateTimeField(auto_now_add=True)  # 날짜 자동 업데이트
     updated = models.DateTimeField(auto_now=True)
+    objects = managers.CustomModelManager()
 
     # 추상 모델은 데이터베이스에 등록 되지 않는다.
     class Meta:
--- reviews/models.py
+++ reviews/models.py
@@ -1,16 +1,27 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 from core import models as core_models
 
 # Create your models here.
 class Review(core_models.TimeStampedModel):
 
     review = models.TextField()
-    accuracy = models.IntegerField()
-    communication = models.IntegerField()
-    cleanliness = models.IntegerField()
-    location = models.IntegerField()
-    check_in = models.IntegerField()
-    value = models.IntegerField()
+    accuracy = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    communication = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    cleanliness = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    location = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    check_in = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    value = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     user = models.ForeignKey(
         "users.User", related_name="reviews", on_delete=models.CASCADE
     )
@@ -33,4 +44,7 @@ class Review(core_models.TimeStampedModel):
 
         return round(avg, 2)
 
-    rating_average.short_description = "AVG"
+    rating_average.short_description = "Avg."
+
+    class Meta:
+        ordering = ("-created",)
